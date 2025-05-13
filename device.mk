dc6a5e38535a62203b97e7587d10cfcd818fa5a1
Samuel Kendall <ltnam359@gmail.com>
2025-05-13 22:03:40 +0330

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,6 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 PRODUCT_RELEASE_NAME := e1s
 DEVICE_PATH := device/samsung/e1s
 
