f4a958f93810cae1fee66c4fd31a1ff0cd145d62
Samuel Kendall <ltnam359@gmail.com>
2025-05-13 22:02:39 +0330

    Update device.mk

--- device.mk
+++ device.mk
@@ -13,10 +13,25 @@ $(call inherit-product-if-exists, $(DEVICE_PATH)/fox_$(PRODUCT_RELEASE_NAME).mk)
 
 LOCAL_PATH := device/samsung/e1s
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock \
     fastbootd
+
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.recovery.s5e9945.rc \
+    init.recovery.samsung.rc \
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
